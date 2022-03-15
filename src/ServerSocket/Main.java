7f5c9763e1553a55db36d0c7cea7e16aa3130932
Ivan Muhammad Nizar <ivannizar@gmail.com>
2022-03-15 12:28:57 +0700

    change icon

--- src/ServerSocket/Main.java
+++ src/ServerSocket/Main.java
@@ -126,7 +126,7 @@ public class Main {
                                     <meta charset="UTF-8">
                                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
                                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                    <link rel="icon" type="image/x-icon" href="https://pngimg.com/uploads/letter_p/letter_p_PNG46.png">
+                                    <link rel="icon" type="image/x-icon" href="https://i.pinimg.com/736x/96/0d/f4/960df438f758d552e7300b7b601ac3c7.jpg">
                                     <title>FILE LIST</title>
                 
                                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -194,7 +194,7 @@ public class Main {
                                     <meta charset="UTF-8">
                                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
                                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                    <link rel="icon" type="image/x-icon" href="https://pngimg.com/uploads/letter_p/letter_p_PNG46.png">
+                                    <link rel="icon" type="image/x-icon" href="https://i.pinimg.com/736x/96/0d/f4/960df438f758d552e7300b7b601ac3c7.jpg">
                                     <title>FILE LIST</title>
                 
                                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
